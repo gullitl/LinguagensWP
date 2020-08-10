213927bfd76a55539d4b99a05721f135dee575b8
plusembo <plam.l@live.fr>
2020-08-09 23:14:59 -0400

    Create Descricao field

--- LinguagensWP/Models/Linguagem.cs
+++ LinguagensWP/Models/Linguagem.cs
@@ -8,12 +8,12 @@ namespace Asp.LinguagensWP.Models
         [Key]
         public int LinguagemId { get; set; }
         [Required(ErrorMessage ="Campo Nome obrigatório.")]
-        [DisplayName("Full Name")]
-        public string FullName { get; set; }
+        [DisplayName("Nome")]
+        public string Nome { get; set; }
         [DisplayName("Emp. Code")]
         public string EmpCode { get; set; }
         public string Position { get; set; }
-        [DisplayName("Office Location")]
-        public string OfficeLocation { get; set; }
+        [DisplayName("Descrição")]
+        public string Descricao { get; set; }
     }
 }
