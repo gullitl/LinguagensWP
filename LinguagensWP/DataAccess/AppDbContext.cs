a0138ef634812f86bd12adc3f322f75baf906550
plusembo <plam.l@live.fr>
2020-08-09 22:00:50 -0400

    First commit

---
+++ LinguagensWP/DataAccess/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Asp.LinguagensWP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinguagensWP.DataAccess {
+    public class AppDbContext : DbContext {
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) {
+            base.OnConfiguring(options);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ LinguagensWP/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asp.LinguagensWP.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage ="Campo Nome obrigatório.")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Emp. Code")]
+        public string EmpCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
